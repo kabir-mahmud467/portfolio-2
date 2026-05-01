f9b56459c843ac03ffe1a478a63deaff7ddcbdd8
Kabir Mahmud <kabirmahmud120706@gmail.com>
2026-05-01 16:30:38 +0600

    test

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
         projects: resolve(__dirname, 'projects/index.html'),
         social: resolve(__dirname, 'social/index.html'),
         blog: resolve(__dirname, 'blog/index.html'),
+        resources: resolve(__dirname, 'resources/index.html'),
       },
       output: {
         manualChunks(id) {
